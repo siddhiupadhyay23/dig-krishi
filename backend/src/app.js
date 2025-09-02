f6d1acbe29f6dd6f66a215d2d1818e75dd99395b
dev-787 <notexotic555@gmail.com>
2025-09-02 21:54:22 +0530

    register login

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const cookieParser = require("cookie-parser")
+const connectToDb = require("./db/db")
+
+//Routes
+const authRoutes = require("./routes/auth.routes")
+const chatRoutes = require("./routes/chat.routes")
+
 const app = express();
+connectToDb()
 
+//using middlewares
+app.use(express.json())
+app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+//using Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/chat",chatRoutes);
 
 module.exports = app;
