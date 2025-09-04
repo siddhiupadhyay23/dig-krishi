060c18529a121d96375211590e9f29134bdb21ef
dev-787 <notexotic555@gmail.com>
2025-09-04 13:48:11 +0530

    some changes

--- frontend/src/components/Working.jsx
+++ frontend/src/components/Working.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Working.scss';
 import section3 from '../assets/section3.jpg';
+import More from './More';
 
 const Working = () => {
   return (
     <div className="working-section">
       <img src={section3} alt="Working Section" className="working-image" />
+      <More />
     </div>
   );
 };
